0016c41dae8def70d0cac4001f5ef9c028e04eaf
keshgurung <k_grg@hotmail.com>
2022-04-05 01:26:00 +0100

    mongodb connected

---
+++ server/config/environment.js
@@ -0,0 +1,3 @@
+export const dbURI = 'mongodb://localhost:27017/ukmaroom'
+export const port = 4000
+export const secret = 'abrakadabara'
--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
 const app = express ()
-const port = 4000
+import { port, dbURI } from './config/environment.js'
+
+
 
 const startServers = async() => {
   try{
+
+    // mongoose connect
+    await mongoose.connect(dbURI)
+    console.log('db connected success')
+
     app.listen(port, ()=> 
     console.log(`server up and running on port ${port}`)
     )
   }
   catch {
-    console.log('somethinf is wrong', err)
+    console.log('something is wrong', err)
   }
 }
 
