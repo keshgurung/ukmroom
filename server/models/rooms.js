bb1588e56c425c014615a5243fdd55d3728a3afd
keshgurung <k_grg@hotmail.com>
2022-04-09 19:53:05 +0100

    room schema final edit

--- server/models/rooms.js
+++ server/models/rooms.js
@@ -1,19 +1,29 @@
+import { text } from 'express'
 import mongoose from 'mongoose'
 import uniqueValidator from 'mongooseUniqueValidator'
 
 const roomSchema = new mongoose.Schema(
   {
-    type: { type: String, required: true },
-    title: { type: String, required: true },
-    location: { type: String, required: true },
-    text: { type: String, required: true, maxlength: 300 },
-    price: { type: Number, required: true, min: 1, max: 3 },
-    valueRating: { type: Number, required: true, min: 1, max: 5 },
-    qualityRating: { type: Number, required: true, min: 1, max: 5 },
+    //about the property
+    addressFirst: { type: String, required: true },
+    addressCity: { type: String, required: true },
+    addressPostalCode: { type: String, required: true },
+    propertyType: { type: String, required: true },
+    totalBedrooms: {type: Number, required: true},
+    totalBathrooms: {type: Number, required: true},
+    totalRoomsToRent: {type: Number, required: true},
+    propertyAmenities: [{type: String, required: true,}],
+
+    // About the room
+    roomTitle: {type: text, required: true},
+    roomDescription: {type: text, required: true},
+    rentPerMonth: {type: Number, required: true},
+    utilityCost: {type: Boolean, required: true, default: false},
+    roomType: {type: String, required: true},
+    roomStatus: {type: String, required: true},
+    ensuiteBathroom: {type: Boolean, required: true, default: false},
+    // qualityRating: { type: Number, required: true, min: 1, max: 5 },
     owner: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
-    city: { type: String, required: true },
-    cityname: { type: String, required: true },
-    ratings: [ ratingsSchema ]
   },
   {
     timestamps: true,
